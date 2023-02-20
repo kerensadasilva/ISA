1ba64157d1ef8e4fb02e8363ca21c29800a82a5c
bca <bca@Bcalab4-m19>
2023-02-20 10:48:56 +0530

    first commit

--- src/Grader.java
+++ src/Grader.java
@@ -1,8 +1,18 @@
 
 public class Grader {
+	
+	int total_marks;
+	String roll_num;
+	
+	Grader(int total_marks, String roll_num)
+	{
+		this.total_marks = total_marks;
+		this.roll_num = roll_num;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Grader g = new Grader (70, "R/BCA-777");
 
 	}
 
